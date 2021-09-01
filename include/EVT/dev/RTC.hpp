ec9f766b69e1825460401382b86a97afe1e6b9b5
Collin Bolles <collinbolles@gmail.com>
2021-08-31 21:56:59 -0400

    Begin to create RTC interface

---
+++ include/EVT/dev/RTC.hpp
@@ -0,0 +1,17 @@
+#ifndef _EVT_RTC_
+#define _EVT_RTC_
+
+namespace EVT::core::DEV {
+
+/**
+ * The RTC is the real time clock interface. This class represents features
+ * common acrross real time clocks.
+ */
+class RTC {
+
+
+};
+
+}  // namespace EVT::core::DEV
+
+#endif
