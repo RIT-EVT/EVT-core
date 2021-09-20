ba65ce2ea158a9756a52d360256f8990d5b7b44f
F. Daniel Popp <ddp5730@rit.edu>
2021-09-20 19:21:54 -0400

    Small Updates

--- src/dev/platform/f3xx/f302x8/Timerf302x8.cpp
+++ src/dev/platform/f3xx/f302x8/Timerf302x8.cpp
@@ -1,4 +1,9 @@
 #include <EVT/dev/platform/f3xx/f302x8/Timerf302x8.hpp>
+#include <HALf3/stm32f3xx_hal_conf.h>
+
+extern "C" void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim) {
+    // TODO
+}
 
 namespace EVT::core::DEV {
 
