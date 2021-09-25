d4ebbf7faee4cad1a17b99e51303cf372b441d25
cbolles <collinbolles@gmail.com>
2021-09-25 12:13:18 -0400

    Theoretical implementation of CANopen stack driver

---
+++ include/EVT/io/CANopen.hpp
@@ -0,0 +1,26 @@
+#ifndef _EVT_CANopen_
+#define _EVT_CANopen_
+
+#include <Canopen/co_if.h>
+
+#include <EVT/io/CAN.hpp>
+
+namespace EVT::core::IO {
+
+   /**
+    * Get an instance of the CAN driver that can be used with the CANopen
+    * stack. This will populate a struct with function pointers that can
+    * handle CAN operations.
+    *
+    * @param can[in] The CAN interface that will be used for the stack driver
+    * @param canDriver[out] The CANopen stack driver to populate
+    * @return pointer to the CAN driver
+    */
+    void getCANopenDriver(CAN& can, CO_IF_CAN_DRV* canDriver);
+
+
+
+}  // namespace EVT::core::IO
+
+
+#endif
