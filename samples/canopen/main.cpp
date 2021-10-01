f0944a4865306da246fc3bf7a0d5fb44179df3b3
cbolles <collinbolles@gmail.com>
2021-10-01 19:06:54 -0400

    CANopen starting without errors

--- samples/canopen/main.cpp
+++ samples/canopen/main.cpp
@@ -25,15 +25,34 @@ namespace time = EVT::core::time;
 // Data that will be accessible over CAN. This will be application specific
 // and ideally not be global variables
 ///////////////////////////////////////////////////////////////////////////////
-uint8_t sampleData;
+uint8_t sampleData = 0;
+uint8_t DemoErrReg = 0;
+uint8_t DemoString[] = "TEST";    /* string memory */
+uint32_t syncId = 0x80;
+
+CO_OBJ_STR DemoStringObj {
+    .Offset = 0,
+    .Start = &DemoString[0],
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 // CAN object dictionary. This represents the data that is exposed by this
 // device on the CAN network
 ///////////////////////////////////////////////////////////////////////////////
 CO_OBJ_T canObjectDictionary[] = {
-    { CO_KEY(0x1001, 0, CO_UNSIGNED8|CO_OBJ____RW), 0, (uintptr_t)sampleData }
+    { CO_KEY(0x1005, 0, CO_UNSIGNED32|CO_OBJ____R_), 0, (uintptr_t)&syncId },
+    { CO_KEY(0x1018, 1, CO_STRING|CO_OBJ____R_), CO_TSTRING, (uintptr_t)&DemoStringObj },
+    { CO_KEY(0x1018, 2, CO_STRING|CO_OBJ____R_), CO_TSTRING, (uintptr_t)&DemoStringObj },
+    { CO_KEY(0x1018, 3, CO_STRING|CO_OBJ____R_), CO_TSTRING, (uintptr_t)&DemoStringObj },
+    { CO_KEY(0x1018, 4, CO_STRING|CO_OBJ____R_), CO_TSTRING, (uintptr_t)&DemoStringObj },
+
+
+
+    { CO_KEY(0x1234, 0, CO_UNSIGNED8|CO_OBJ____RW), 0, (uintptr_t)sampleData }
 };
+
+
+    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Callbacks
@@ -55,7 +74,7 @@ extern "C" int16_t COLssLoad(uint32_t *baudrate, uint8_t *nodeId) {
 }
 
 extern "C" void CONmtModeChange(CO_NMT *nmt, CO_MODE mode) {
-
+    uart.printf("mode is now: %d\r\n", mode);
 }
 
 extern "C" void CONmtHbConsEvent(CO_NMT *nmt, uint8_t nodeId) {
@@ -95,7 +114,6 @@ int main() {
     IO::init();
 
     // Intialize peripherals
-    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
     IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>();
     
     // Start RTC and set the default time
@@ -111,6 +129,9 @@ int main() {
     DEV::RTCf302x8 rtc;
     rtc.setTime(time);
 
+    // Not sure if this is needed
+    uint8_t sdoBuffer[50];
+
     ///////////////////////////////////////////////////////////////////////////
     // Setup CAN configuration, this handles making drivers, applying settings.
     // And generally creating the CANopen stack node which is the interface
@@ -118,43 +139,46 @@ int main() {
     ///////////////////////////////////////////////////////////////////////////
     // Make drivers
     CO_IF_DRV canStackDriver;
-    getCANopenDriver(can, (CO_IF_CAN_DRV*)(canStackDriver.Can)); 
-    getCANopenTimerDriver(rtc, (CO_IF_TIMER_DRV*)canStackDriver.Timer);
-    IO::getCANopenNVMDriver((CO_IF_NVM_DRV*)canStackDriver.Nvm);
+
+    CO_IF_CAN_DRV canDriver;
+    CO_IF_TIMER_DRV timerDriver;
+    CO_IF_NVM_DRV nvmDriver;
+
+    getCANopenDriver(can, &canDriver); 
+    getCANopenTimerDriver(rtc, &timerDriver);
+    IO::getCANopenNVMDriver(&nvmDriver);
+
+    canStackDriver.Can = &canDriver;
+    canStackDriver.Timer = &timerDriver;
+    canStackDriver.Nvm = &nvmDriver;
     
     CO_NODE_SPEC canSpec = {
         .NodeId = 0x01,
         .Baudrate = 500000,
         .Dict = &canObjectDictionary[0],
-        .DictLen = 1,
+        .DictLen = 5,
         .EmcyCode = NULL,
         .TmrMem = NULL,
         .TmrNum = 0,
         .TmrFreq = 8000000,
         .Drv = &canStackDriver,
-        .SdoBuf = NULL
+        .SdoBuf = &sdoBuffer[0],
     };
 
     CO_NODE canNode;
 
     CONodeInit(&canNode, &canSpec);
+    CONodeStart(&canNode);
 
     time::wait(500);
 
-    IO::ADC& adc0 = IO::getADC<IO::Pin::PA_0>();
-    IO::ADC& adc1 = IO::getADC<IO::Pin::PA_1>();
+    uart.printf("Error: %d\r\n", CONodeGetErr(&canNode));
 
     while (1) {
-        uart.printf("--------------------\r\n");
-        uart.printf("ADC0 : %.2fV\r\n", adc0.read());
-        uart.printf("ADC0: %.2f%%\r\n", adc0.readPercentage() * 100);
-        uart.printf("ADC0 raw: %d\r\n\r\n", adc0.readRaw());
-
-        uart.printf("ADC1: %.2fV\r\n", adc1.read());
-        uart.printf("ADC1: %.2f%%\r\n", adc1.readPercentage());
-        uart.printf("ADC1 raw: %d\r\n", adc1.readRaw());
-
-        uart.printf("--------------------\r\n\r\n");
-        time::wait(500);
+        if(sampleData != 0) {
+            uart.printf("Value of my number: %d\n\r", sampleData);
+        }
+        CONodeProcess(&canNode);
+        time::wait(10);
     }
 }
