b47f53c0e3e8150ea7dc7d8354b40166d34359eb
zks1596_evt <zks1596@rit.edu>
2021-10-02 01:06:58 -0400

    UART functioning correctly after namespace error resolved

--- src/io/platform/f3xx/f302x8/UARTf302x8.cpp
+++ src/io/platform/f3xx/f302x8/UARTf302x8.cpp
@@ -50,16 +50,12 @@ UARTf302x8::UARTf302x8(Pin txPin, Pin rxPin, uint32_t baudrate)
             if (!(__HAL_RCC_USART1_IS_CLK_ENABLED()))
                 __HAL_RCC_USART1_CLK_ENABLE();
 
-//            gpioInit.Alternate = GPIO_AF7_USART1;
-
             break;
         case 2:
             halUART.Instance = USART2;
 
             if (!(__HAL_RCC_USART2_IS_CLK_ENABLED()))
                 __HAL_RCC_USART2_CLK_ENABLE();
-
-//            gpioInit.Alternate = GPIO_AF7_USART2;
 
             break;
         case 3:
@@ -68,45 +64,10 @@ UARTf302x8::UARTf302x8(Pin txPin, Pin rxPin, uint32_t baudrate)
             if (!(__HAL_RCC_USART3_IS_CLK_ENABLED()))
                 __HAL_RCC_USART3_CLK_ENABLE();
 
-//            gpioInit.Alternate = GPIO_AF7_USART3;
-
             break;
         default:
             break;
     }
-
-    // Pin myPins[2] = {txPin, rxPin};
-
-    // gpioInit.Pin = static_cast<uint32_t>(1 <<
-    //         (static_cast<uint32_t>(myPins[0]) & 0x0F));
-    // gpioInit.Pin |= static_cast<uint32_t>(1 <<
-    //         (static_cast<uint32_t>(myPins[1]) & 0x0F));
-    // gpioInit.Mode = GPIO_MODE_AF_PP;
-    // gpioInit.Pull = GPIO_NOPULL;
-    // gpioInit.Speed = GPIO_SPEED_FREQ_HIGH;
-
-    // for (uint8_t i = 0; i < 2; i++) {
-    //     switch ((static_cast<uint8_t>(myPins[i]) & 0xF0) >> 4) {
-    //         case 0x0:
-    //             __HAL_RCC_GPIOA_CLK_ENABLE();
-    //             HAL_GPIO_Init(GPIOA, &gpioInit);
-    //             break;
-    //         case 0x1:
-    //             __HAL_RCC_GPIOB_CLK_ENABLE();
-    //             HAL_GPIO_Init(GPIOB, &gpioInit);
-    //             break;
-    //         case 0x2:
-    //             __HAL_RCC_GPIOC_CLK_ENABLE();
-    //             HAL_GPIO_Init(GPIOC, &gpioInit);
-    //             break;
-    //         case 0x3:
-    //             __HAL_RCC_GPIOD_CLK_ENABLE();
-    //             HAL_GPIO_Init(GPIOD, &gpioInit);
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // }
 
     GPIOf302x8::gpioStateInit(&gpioInit, myPins, numOfPins, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH, alt_id);
 
