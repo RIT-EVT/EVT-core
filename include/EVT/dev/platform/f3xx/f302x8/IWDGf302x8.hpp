c4662e9a619dfe0639d77565facaa32a8056e3fd
Drew Pawluckie <drewpawluckie@gmail.com>
2021-10-02 11:08:57 -0400

    Implemented methods for controlling iwdg

--- include/EVT/dev/platform/f3xx/f302x8/IWDGf302x8.hpp
+++ include/EVT/dev/platform/f3xx/f302x8/IWDGf302x8.hpp
@@ -7,11 +7,24 @@
 
 namespace EVT::core::DEV {
 
+
     class IWDGf302x8: public IWDG {
     public:
+
+        /**
+         * Constructor for the IWDG.
+         * @param windowSize Window size that's compared to the counter value.
+         * Must be a value between 0 and 4095
+         */
         IWDGf302x8(uint32_t windowSize);
+
+        /**
+         * Refreshes the IWDG to avoid a system reset
+         */
         void refresh();
+
     private:
+        /** Instance of the HAL IWDG */
         IWDG_HandleTypeDef halIWDG;
     };
 
--- src/dev/platform/f3xx/f302x8/IWDGf302x8.cpp
+++ src/dev/platform/f3xx/f302x8/IWDGf302x8.cpp
@@ -2,6 +2,19 @@
 
 namespace EVT::core::DEV {
 
+IWDGf302x8::IWDGf302x8(uint32_t windowSize) {
+    halIWDG.Instance = IWDG1;
+    halIWDG.Init.Window = windowSize;
 
+    // Numbers generated from STMCubeMX
+    halIWDG.Init.Reload = 4095;
+    halIWDG.Init.Prescaler = IWDG_PRESCALER_4;
+
+    HAL_IWDG_Init(&halIWDG);
+}
+
+void IWDGf302x8::refresh() {
+    HAL_IWDG_Refresh(&halIWDG);
+}
 
 } // namespace EVT::core::DEV
