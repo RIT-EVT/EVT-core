7f8c4fb8f20aa51c820387a979cd18e691ac8d77
Collin Bolles <collinbolles@gmail.com>
2021-10-08 23:04:34 -0400

    Move object dictionary declaration into header

--- samples/canopen/TestCanNode.hpp
+++ samples/canopen/TestCanNode.hpp
@@ -50,11 +50,6 @@ public:
 
 private:
     /**
-     * The number of elements that will be stored by the object dictionary.
-     * This will be known.
-     */
-    static constexpr uint8_t OBJECT_DIRECTIONARY_SIZE = 15;
-    /**
      * The node ID used to identify the device on the CAN network.
      */
     static constexpr uint8_t NODE_ID = 0x01;
@@ -68,11 +63,118 @@ private:
     uint8_t sampleData;
 
     /**
+     * Have to know the size of the object dictionary for initialization
+     * process.
+     */
+    static constexpr uint8_t OBJECT_DIRECTIONARY_SIZE = 16;
+
+    /**
      * The object dictionary itself. Will be populated by this object during
      * construction.
      *
      * The plus one is for the special "end of dictionary" marker.
      */
-    CO_OBJ_T objectDictionary[OBJECT_DIRECTIONARY_SIZE + 1];
+    CO_OBJ_T objectDictionary[OBJECT_DIRECTIONARY_SIZE + 1] = {
+        // Sync ID, defaults to 0x80
+        { CO_KEY(0x1005, 0, CO_UNSIGNED32|CO_OBJ_D__R_), 0, (uintptr_t)0x80 },
+
+        // Information about the hardware, hard coded sample values for now
+        // 1: Vendor ID
+        // 2: Product Code
+        // 3: Revision Number
+        // 4: Serial Number
+        {
+            .Key = CO_KEY(0x1018, 1, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0x10
+        },
+        {
+            .Key = CO_KEY(0x1018, 2, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0x11
+        },
+        {
+            .Key = CO_KEY(0x1018, 3, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0x12
+        },
+        {
+            .Key = CO_KEY(0x1018, 4, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0x13
+        },
+
+        // SDO CAN message IDS.
+        // 1: Client -> Server ID, default is 0x600 + NODE_ID
+        // 2: Server -> Client ID, default is 0x580 + NODE_ID
+        {
+            .Key = CO_KEY(0x1200, 1, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0x600 + NODE_ID
+        },
+        {
+            .Key = CO_KEY(0x1200, 2, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0x580 + NODE_ID
+        },
+
+        // TPDO0 settings
+        // 0: The TPDO number, default 0
+        // 1: The COB-ID used by TPDO0, provided as a function of the TPDO number
+        // 2: How the TPO is triggered, default to manual triggering
+        // 3: Inhibit time, defaults to 0
+        // 5: Timer trigger time in 1ms units, 0 will disable the timer based triggering
+        {
+            .Key = CO_KEY(0x1800, 0, CO_UNSIGNED8|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0
+        },
+        {
+            .Key = CO_KEY(0x1800, 1, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)CO_COBID_TPDO_DEFAULT(0)
+        },
+        {
+            .Key = CO_KEY(0x1800, 2, CO_UNSIGNED8|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0xFE
+        },
+        {
+            .Key = CO_KEY(0x1800, 3, CO_UNSIGNED16|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)0
+        },
+        {
+            .Key = CO_KEY(0x1800, 5, CO_UNSIGNED16|CO_OBJ_D__R_),
+            .Type = CO_TEVENT,
+            .Data = (uintptr_t)0
+        },
+
+        // TPDO0 mapping, determins the PDO messages to send when TPDO1 is triggered
+        // 0: The number of PDO message associated with the TPDO
+        // 1: Link to the first PDO message
+        // n: Link to the nth PDO message
+        {
+            .Key = CO_KEY(0x1A00, 0, CO_UNSIGNED8|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = (uintptr_t)1
+        },
+        {
+            .Key = CO_KEY(0x1A00, 1, CO_UNSIGNED32|CO_OBJ_D__R_),
+            .Type = 0,
+            .Data = CO_LINK(0x2100, 0, 8) // Link to sample data position in dictionary
+        },
+
+        // User defined data, this will be where we put elements that can be
+        // accessed via SDO and depeneding on configuration PDO
+        {
+            .Key = CO_KEY(0x2100, 0, CO_UNSIGNED8|CO_OBJ___PRW),
+            .Type = 0,
+            .Data = (uintptr_t)&sampleData
+        },
+
+        // End of dictionary marker
+        CO_OBJ_DIR_ENDMARK
+    };
 
 };
