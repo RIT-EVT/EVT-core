c933f0578e9c85ede85b0c98653b46c12d97f062
Aaron <fireninja108@gmail.com>
2021-10-21 20:52:36 -0400

    Create a new header for button LEDs. Read state is now private. TODO: Add list of LEDs

--- include/EVT/dev/button.hpp
+++ include/EVT/dev/button.hpp
@@ -25,40 +25,28 @@ enum class Press
     PRESSED = 1u,
 };
 
-/**
- * Create a new button interface
- *
- */
-Button(GPIO button);
+    /**
+     * Create a new button interface
+     *
+     */
+    Button(GPIO button);
 
-/**
- * Reads the state of the button 
- * 
- * @return The button state.
- */
-virtual Press readButton();
+    /**
+     * Reads the state of the button 
+     * 
+     * @return The button state.
+     */
+    Press readButton();
 
-/**
- * Confirms a button press due to bouncing signal
- * 
- * @return Confirmation of a press  
- */
-virtual void debounce();
+    /**
+     * Confirms a button press due to bouncing signal
+     * 
+     * @return Confirmation of a press  
+     */
+    Press debounce();
 
 
-/**
- * Tells an LED to be on or off
- * 
- * @return The state to write to the LED
- */
-virtual void writeLED();
 
-/**
- * Read whether an LED is on or off
- * 
- * @return ledState Read whether the LED is on or off 
- */
-virtual Press readLED(); // May need to split since some buttons may not use LEDs
 
 
 protected:
