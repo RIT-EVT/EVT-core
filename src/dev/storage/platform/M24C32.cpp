a4a014f52630d5236c0cb6f167e209480ecf4acb
cbolles <collinbolles@gmail.com>
2021-11-13 16:25:43 -0500

    EEPROM Page: Writing on page boundary
    The handle wrapping around writing around the page boundry. Will split
    up write requests to handle writting across multiple pages.

--- src/dev/storage/platform/M24C32.cpp
+++ src/dev/storage/platform/M24C32.cpp
@@ -25,25 +25,38 @@ uint32_t M24C32::readWord(uint32_t address) {
 }
 
 void M24C32::readBytes(uint8_t address, uint8_t* buffer, uint8_t numBytes) {
-    i2c.readMemReg(i2cSlaveAddress, address, buffer, numBytes, MEM_ADDRESS_SIZE);
+    uint16_t currentAddress = address;
+    uint8_t bytesRead = 0;
+
+    uint8_t* currentBufferPtr = buffer;
+
+    while (bytesRead < numBytes) {
+        uint8_t bytesRemaining = numBytes - bytesRead;
+        uint8_t maxBytes = PAGE_SIZE - (address % PAGE_SIZE);
+
+        uint8_t bytesToRead = maxBytes > numBytes ? numBytes : maxBytes;
+
+        i2c.readMemReg(i2cSlaveAddress, currentAddress, currentBufferPtr, bytesToRead, MEM_ADDRESS_SIZE);
+        bytesRead += bytesToRead;
+        currentAddress += bytesRead;
+        currentBufferPtr = &buffer[bytesRead];
+    }
 }
 
 void M24C32::readHalfWords(uint8_t address, uint16_t* buffer, uint8_t numHWords) {
     uint8_t numBytes = numHWords * 2;
     uint8_t tempBuf[numBytes];
-    i2c.readMemReg(i2cSlaveAddress, address, tempBuf, numBytes, MEM_ADDRESS_SIZE);
-    auto* convertedBuf = reinterpret_cast<uint16_t*>(tempBuf);
-    for (int i = 0; i < numHWords; i++)
-        buffer[i] = convertedBuf[i];
+    readBytes(address, tempBuf, numBytes);
+    auto* convertedBuf = reinterpret_cast<uint16_t *>(tempBuf);
+    for (int i = 0; i < numHWords; i++) buffer[i] = convertedBuf[i];
 }
 
 void M24C32::readWords(uint8_t address, uint32_t* buffer, uint8_t numWords) {
     uint8_t numBytes = numWords * 4;
     uint8_t tempBuf[numBytes];
-    i2c.readMemReg(i2cSlaveAddress, address, tempBuf, numBytes, MEM_ADDRESS_SIZE);
-    auto* convertedBuf = reinterpret_cast<uint32_t*>(tempBuf);
-    for (int i = 0; i < numWords; i++)
-        buffer[i] = convertedBuf[i];
+    readBytes(address, tempBuf, numBytes);
+    auto* convertedBuf = reinterpret_cast<uint32_t *>(tempBuf);
+    for (int i = 0; i < numWords; i++) buffer[i] = convertedBuf[i];
 }
 
 void M24C32::writeByte(uint32_t address, uint8_t data) {
@@ -61,18 +74,32 @@ void M24C32::writeWord(uint32_t address, uint32_t data) {
 }
 
 void M24C32::writeBytes(uint8_t address, uint8_t* dataArr, uint8_t numBytes) {
-    i2c.writeMemReg(i2cSlaveAddress, address, dataArr, numBytes, MEM_ADDRESS_SIZE, MAX_WRITE_TIME);
+    uint16_t currentAddress = address;
+    uint8_t bytesWritten = 0;
+
+    uint8_t* currentBufferPtr = dataArr;
+
+    while (bytesWritten < numBytes) {
+        uint8_t bytesRemaining = numBytes - bytesWritten;
+
+        uint8_t maxBytes = PAGE_SIZE - (address % PAGE_SIZE);
+
+        uint8_t bytesToWrite = maxBytes > numBytes ? numBytes : maxBytes;
+
+        i2c.writeMemReg(i2cSlaveAddress, currentAddress, currentBufferPtr, bytesToWrite, MEM_ADDRESS_SIZE, MAX_WRITE_TIME);
+        bytesWritten += bytesToWrite;
+        currentAddress += bytesWritten;
+        currentBufferPtr = &dataArr[bytesWritten];
+    }
 }
 
 void M24C32::writeHalfWords(uint8_t address, uint16_t dataArr[], uint8_t numHWords) {
-    auto* tempDataArr = reinterpret_cast<uint8_t*>(dataArr);
-    i2c.writeMemReg(i2cSlaveAddress, address, tempDataArr, numHWords * 2, MEM_ADDRESS_SIZE,
-                    MAX_WRITE_TIME);
+    auto* tempDataArr = reinterpret_cast<uint8_t *>(dataArr);
+    writeBytes(address, tempDataArr, numHWords * 2);
 }
 
 void M24C32::writeWords(uint8_t address, uint32_t* dataArr, uint8_t numWords) {
-    auto* tempDataArr = reinterpret_cast<uint8_t*>(dataArr);
-    i2c.writeMemReg(i2cSlaveAddress, address, tempDataArr, numWords * 4, MEM_ADDRESS_SIZE,
-                    MAX_WRITE_TIME);
+    auto* tempDataArr = reinterpret_cast<uint8_t *>(dataArr);
+    writeBytes(address, tempDataArr, numWords * 4);
 }
-}// namespace EVT::core::DEV
+}
