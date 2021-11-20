2fbd878c1fee7117f3b582f83a5ea333d3f4f288
mjh9585 <69865851+mjh9585@users.noreply.github.com>
2021-11-20 12:35:05 -0500

    Full SPI test code

--- samples/spi/arduino_source/spi_slave_test/spi_slave_test.ino
+++ samples/spi/arduino_source/spi_slave_test/spi_slave_test.ino
@@ -1,17 +1,24 @@
-// ====================================================================
-// Arduino code example for SPI Slave Mode
-// Read unsigned short (two bytes) from SPI, send word to serial port
-// On 16 MHz Arduino, can work at > 500 words per second
-// J.Beale July 19 2011
-// ====================================================================
 
-#define SCK_PIN   52  // D13 = pin19 = PortB.5
-#define MISO_PIN  50  // D12 = pin18 = PortB.4
-#define MOSI_PIN  51  // D11 = pin17 = PortB.3
-#define SS_PIN    53  // D10 = pin16 = PortB.2
+#define SCK_PIN   52
+#define MISO_PIN  50
+#define MOSI_PIN  51
+#define SS_PIN    53
 
 #define UL unsigned long
 #define US unsigned short
+
+#define REG_READ_BYTE       0x10
+#define REG_WRITE_8BYTES    0x40
+#define REG_READ_8BYTES     0x11
+#define REG_WRITE_BYTE      0x20
+#define REG_READ_BYTE2      0x28
+
+#define READ_SINGLE_RESPONSE  0x82
+#define READ_REG_RESPONSE     0xE5
+
+const uint8_t BYTE_MULTIPLE[] = {0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F};
+
+uint8_t dataArray[20];
 
 void SlaveInit(void) {
   // Set MISO output, all others input
@@ -39,48 +46,134 @@ void SlaveInit(void) {
 // ================================================================
 // read in short as two bytes, with high-order byte coming in first
 // ================================================================
-unsigned short Read2Bytes(void) {
-    union {
-    unsigned short svar;
-    byte c[2];
-  } w;        // allow access to 2-byte word, or separate bytes
+byte readByte(void) {  
+  while(!(SPSR & (1<<SPIF))) ; // SPIF bit set when 8 bits received
+  return SPDR; // send back unsigned short value
+}
+
+void writeByte(uint8_t data){
+  SPDR = data;
+  while(!(SPSR & (1<<SPIF))); 
+}
+
+int handleReadMultiple(){
+  int count = 0;
+  for(int i = 0; i<8; i++){
+    if(digitalRead(SS_PIN)==1){
+      break;
+    }
+    dataArray[count] = BYTE_MULTIPLE[i];
+    writeByte(BYTE_MULTIPLE[i]);
+    count++;
+  }
+  return count;
+}
+
+int handleWriteMultiple(){
+  int count = 0;
+  while (digitalRead(SS_PIN)==0) {
+    if(SPSR & (1<<SPIF)){
+      dataArray[count] = SPDR;
+    }
+  }
+  return count;
+}
+
+void printDataArray(uint8_t data[], int count){
+  Serial.print("Data: ");
+  for(int i=0; i<count; i++){
+    Serial.print("0x");
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+}
+
+// ================================================================
+// read in short as two bytes, with high-order byte coming in first
+// ================================================================
+//unsigned short Read2Bytes(void) {
+//    union {
+//    unsigned short svar;
+//    byte c[2];
+//  } w;        // allow access to 2-byte word, or separate bytes
+//  
+//  while(!(SPSR & (1<<SPIF))) ; // SPIF bit set when 8 bits received
+//  w.c[1] = SPDR;               // store high-order byte
+//  while(!(SPSR & (1<<SPIF))) ; // SPIF bit set when 8 bits received
+//  w.c[0] = SPDR;               // store low-order byte
+//  return (w.svar); // send back unsigned short value
+//}
+
+void parseReg(uint8_t reg){
+  Serial.print("Requested register: 0x");
+  Serial.println(reg,HEX);
+  Serial.print("Name: ");
+  int count = 0;
+  switch(reg){
+    case REG_READ_BYTE:
+      Serial.println("Read single");
+      Serial.print("Sending: 0x");
+      Serial.println(READ_SINGLE_RESPONSE,HEX);
+      writeByte(READ_SINGLE_RESPONSE);
+      break;
+    case REG_WRITE_8BYTES:
+      Serial.println("Write 8 bytes");
+      count = handleWriteMultiple();
+      Serial.print("Recived ");
+      Serial.print(count);
+      Serial.println(" bytes");
+      printDataArray(dataArray,count);
+      break;
+    case REG_READ_8BYTES:
+      Serial.println("Read 8 bytes");
+      Serial.print("Sending ");
+      count = handleReadMultiple();
+      Serial.print(count);
+      Serial.println(" bytes");
+      printDataArray(dataArray,count);
+      break;
+    case REG_WRITE_BYTE: 
+      Serial.println("Write register");
+      Serial.print("Data: ");
+      byte b = readByte();
+      Serial.println(b, HEX);
+      break;
+    case REG_READ_BYTE2: 
+      Serial.println("Read register");
+      Serial.print("Sending: ");
+      Serial.println(READ_REG_RESPONSE,HEX);
+      writeByte(READ_REG_RESPONSE);
+      break;
+    default:
+      Serial.println("None");
+      break;
+  }
   
-  while(!(SPSR & (1<<SPIF))) ; // SPIF bit set when 8 bits received
-  w.c[1] = SPDR;               // store high-order byte
-  while(!(SPSR & (1<<SPIF))) ; // SPIF bit set when 8 bits received
-  w.c[0] = SPDR;               // store low-order byte
-  return (w.svar); // send back unsigned short value
+  Serial.println();
 }
 
 void setup() {
   Serial.begin(115200);
   SlaveInit();  // set up SPI slave mode
   delay(10);
-  Serial.println("SPI port reader v0.1"); 
+  Serial.println("SPI port test"); 
 }
 
 // ============================================================
 // main loop: read in short word (2 bytes) from external SPI master
 // and send value out via serial port
-// On 16 MHz Arduino, works at > 500 words per second
 // ============================================================
 void loop() {
-  unsigned short word1;
-  byte flag1;
+  byte reg;
 
-     // SS_PIN = Digital_10 = ATmega328 Pin 16 =  PORTB.2
+    // SS_PIN = Digital_10 = ATmega328 Pin 16 =  PORTB.2
     // Note: digitalRead() takes 4.1 microseconds
     // NOTE: SS_PIN cannot be properly read this way while SPI module is active!
     while (digitalRead(SS_PIN)==1) {} // wait until SlaveSelect goes low (active)
     
     //SPCR = (1<<SPE)|(0<<DORD)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA)|(0<<SPR1)|(0<<SPR0); // SPI on
-    word1 = Read2Bytes();          // read unsigned short value
+    reg = readByte();
+    parseReg(reg);
     //SPCR = (0<<SPE)|(0<<DORD)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA)|(0<<SPR1)|(0<<SPR0);  // SPI off
-  
-//    float seconds = millis()/1000.0;  // time stamp takes more serial time, of course
-//    Serial.print(seconds,3);   
-//    Serial.print(",");
-    Serial.print(word1,HEX);
-    Serial.println();
 
 }  // end loop()
--- samples/spi/main.cpp
+++ samples/spi/main.cpp
@@ -17,9 +17,14 @@ namespace time = EVT::core::time;
 
 constexpr uint32_t SPI_SPEED = 4000000; // 4MHz
 /** The address of the arduino listening for I2C requests */
-constexpr uint8_t SINGLE_BYTE = 0x40;
-uint8_t BYTE_MULTIPLE[] = {0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F};
-constexpr uint8_t BYTE_MULTIPLE_LENGTH = 8;
+constexpr uint8_t SINGLE_BYTE = 0x10;
+
+constexpr uint8_t READ_MULTIPLE_BYTE = 0x11;
+uint8_t BYTE_MULTIPLE[] = {0x40, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F};
+constexpr uint8_t BYTE_MULTIPLE_LENGTH = 9;
+constexpr uint8_t WRITE_REG = 0x20;
+constexpr uint8_t WRITE_REG_BYTE = 0xF3;
+constexpr uint8_t READ_REG = 0x28;
 
 constexpr uint8_t deviceCount = 1;
 
@@ -38,22 +43,40 @@ int main() {
     IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
     uart.printf("Starting SPI test\n\r");
+    uint8_t byte;
 
     while (1) {
-        uart.printf("sending single byte\n\r");
-        spi.write(0, SINGLE_BYTE);
-        uart.printf("sending multiple bytes\n\r");
-        spi.write(0, BYTE_MULTIPLE, BYTE_MULTIPLE_LENGTH);
-        //uart.printf("sending single byte");
-        //uart.printf("sending single byte");
-//        uart.printf("Requesting first byte\n\r");
-//        uint8_t oValue = i2c.readReg(I2C_SLAVE_ADDR, O_REGISTER);
-//        uart.printf("Reading second bytes\n\r");
-//        uint8_t kValue = i2c.readReg(I2C_SLAVE_ADDR, K_REGISTER);
-//
-//        uart.printf("Bytes Read: %c %c\n\r", oValue, kValue);
-//
-//        // Wait half a second before repeating the test
+    //     uart.printf("Begin test\n\r");
+    //     //write a single byte
+    //     spi.write(0, SINGLE_BYTE);
+    //     uart.printf("sending single byte %X\n\r", SINGLE_BYTE);
+
+    //     //read a single byte
+    //     byte = spi.read(0);
+    //     uart.printf("reading single byte %X\n\r", byte);
+
+    //    //write an array of data
+    //    spi.write(0, BYTE_MULTIPLE, BYTE_MULTIPLE_LENGTH);
+    //    uart.printf("sending %u bytes\n\r", BYTE_MULTIPLE_LENGTH);
+
+    //    //read an array of data
+    //    uint8_t readData[BYTE_MULTIPLE_LENGTH];
+    //    spi.write(0, READ_MULTIPLE_BYTE);
+    //    spi.read(0, readData, BYTE_MULTIPLE_LENGTH-1);
+    //    uart.printf("reading %u bytes\n\r[", BYTE_MULTIPLE_LENGTH-1);
+    //    for(int i = 0; i<BYTE_MULTIPLE_LENGTH; i++){
+    //        uart.printf("%X, ", readData[i]);
+    //    }
+
+       uart.printf("\n\rtesting combined register read and write\n\r");
+
+       spi.writeReg(0, WRITE_REG, WRITE_REG_BYTE);
+       uart.printf("sending register byte %X\n\r", SINGLE_BYTE);
+
+       byte = spi.readReg(0, READ_REG);
+       uart.printf("reading register byte %X\n\r\n\r", byte);
+
+        // Wait five seconds before repeating the test
         time::wait(1000);
     }
 }
