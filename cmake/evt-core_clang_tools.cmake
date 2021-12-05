513d6ac73eaba32639fad4c5a8296a550a3676ca
mjmagee991 <mjmagee991@gmail.com>
2021-12-05 15:57:36 -0500

    Changed Linting to Toggle from CMake Option
    Defining EVT_APPLY_FORMATTING to be true enables project-wide formatting with clang-format
    Only works in linux right now

--- cmake/evt-core_clang_tools.cmake
+++ cmake/evt-core_clang_tools.cmake
@@ -5,19 +5,15 @@ file(GLOB_RECURSE
         *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
         )
 
-message(ATTEMPT)
-if(EVT_CORE_FORMATTING)
-    message(OUTSIDE)
-    # Adding clang-format target if executable is found
-    find_program(CLANG_FORMAT "clang-format")
-    message(${CLANG_FORMAT})
-    if(CLANG_FORMAT)
-        message(INSIDE)
-        execute_process(
-                COMMAND /usr/bin/clang-format-12
-                -i
-                -style=file
-                ${ALL_CXX_SOURCE_FILES}
-        )
-    endif()
+# Adding clang-format target if executable is found
+find_program(CLANG_FORMAT "clang-format")
+message(${CLANG_FORMAT})
+if(CLANG_FORMAT)
+    message(APPLYING)
+    execute_process(
+            COMMAND /usr/bin/clang-format-12
+            -i
+            -style=file
+            ${ALL_CXX_SOURCE_FILES}
+    )
 endif()
