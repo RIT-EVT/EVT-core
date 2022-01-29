5a652b7dbab403588a48f449a6a48138974d9cc0
mjh9585 <69865851+mjh9585@users.noreply.github.com>
2022-01-29 11:00:33 -0500

    added speed constants and documentation

--- include/EVT/io/SPI.hpp
+++ include/EVT/io/SPI.hpp
@@ -7,10 +7,19 @@
 #define SPI_MSB_FIRST 1
 #define SPI_LSB_FIRST 0
 
-#define SPIMode0 0x01
-#define SPIMode1 0x02
-#define SPIMode2 0x04
-#define SPIMode3 0x08
+#define SPI_MODE0 0x01
+#define SPI_MODE1 0x02
+#define SPI_MODE2 0x04
+#define SPI_MODE3 0x08
+
+#define SPI_SPEED_4MHZ 4000000
+#define SPI_SPEED_2MHZ 2000000
+#define SPI_SPEED_1MHZ 1000000
+#define SPI_SPEED_500KHZ 500000
+#define SPI_SPEED_250KHZ 250000
+#define SPI_SPEED_125KHZ 125000
+#define SPI_SPEED_62KHZ 62500
+#define SPI_SPEED_31KHZ 31250
 
 
 namespace EVT::core::IO {
@@ -60,9 +69,6 @@ namespace EVT::core::IO {
          */
         virtual uint8_t read() = 0;
 
-//        void write(uint8_t device, uint8_t byte);
-//
-//        uint8_t read(uint8_t device);
         /**
          * writes an array of bytes to the SPI device. Call startTransmission() first to initiate device communication.
          * @param bytes an array of bytes of length n to write to SPI device
@@ -77,10 +83,47 @@ namespace EVT::core::IO {
         */
         void read(uint8_t *bytes, uint8_t length);
 
+        /**
+         * writes a byte of data to a register of a device
+         * @param device the device index in the CSPins
+         * @param reg the register address to write to
+         * @param byte the byte of data to write
+         */
         void writeReg(uint8_t device, uint8_t reg, uint8_t byte);
 
+        /**
+         * reads a byte of data from a register from a device
+         * @param device the device index in the CSPins
+         * @param reg the register address to read from
+         * @return the byte of data from the device
+         */
         uint8_t readReg(uint8_t device, uint8_t reg);
 
+         /**
+          * Writes a series of bytes to a device's registers starting at a specific one.
+          * (Device must support a multi-byte write)
+          * @param device the device index in the CSPins
+          * @param reg the register address to start the write at
+          * @param bytes an array of bytes of length n to write to SPI device
+          * @param length the length of the array
+          */
+        void writeReg(uint8_t device, uint8_t reg, uint8_t *bytes, uint8_t length);
+
+        /**
+         * reads a series of bytes from a device's registers starting at a specific one.
+         * @param device the device index in the CSPins
+         * @param reg the register address to start the read from
+         * @param bytes an array of bytes of length n to store the byte from an SPI device
+         * @param length the length of the array
+         */
+        void readReg(uint8_t device, uint8_t reg, uint8_t *bytes, uint8_t length);
+
+        /**
+         * Configures the SPI transmit mode
+         * @param baudRate the baudrate to transmit at (4MHz to 31.25KHz)
+         * @param mode The SPIMode to use when sending (0-3)
+         * @param order MSB first or LSB first
+         */
         virtual void configureSPI(uint32_t baudRate, uint8_t mode, uint8_t order) = 0;
 
     private:
