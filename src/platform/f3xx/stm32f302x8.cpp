4d832bf37ff6d23ef330375dede8bf8629f64203
ddp5730 <ddp5730@rit.edu>
2022-02-04 16:57:54 -0500

    Configured ADC to use slower AHB clock (8MHz) vs PLL Clock (32 MHz)

--- src/platform/f3xx/stm32f302x8.cpp
+++ src/platform/f3xx/stm32f302x8.cpp
@@ -20,23 +20,28 @@ void stm32f302x8_init() {
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL4; // PLL CLK at 8 * 4 = 32 MHz
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
     /** Initializes the CPU, AHB and APB buses clocks
     */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
         | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI; // SYSCLK at 8 MHz (HSI CLK)
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
     HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+
+    // ADC is currently configured to run based on AHB clock.
+    // Enable the PLL Clock below and update 'halADC.Init.ClockPrescaler' to run ADC using faster PLL clock
+    /*
     PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC1;
     PeriphClkInit.Adc1ClockSelection = RCC_ADC1PLLCLK_DIV1;
 
     HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+     */
 
     SysTick_Handler();
 }
