2668f009710b004b92199f763539c19ebb03fd85
mjh9585 <69865851+mjh9585@users.noreply.github.com>
2022-10-01 14:06:36 -0400

    Fix CAN filter and Corrected example TPDO to include node ID

--- samples/canopen/TestCanNode.hpp
+++ samples/canopen/TestCanNode.hpp
@@ -131,7 +131,7 @@ private:
         {
             .Key = CO_KEY(0x1800, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = 0,
-            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0),
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0) + NODE_ID,
         },
         {
             .Key = CO_KEY(0x1800, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
