3bbe3130ef013fd5c6f72cad19233d8abe782751
Rue <xavierfinch05@gmail.com>
2023-10-16 21:18:09 -0400

    Basic functionality implemented and documented

--- include/EVT/dev/encoder.hpp
+++ include/EVT/dev/encoder.hpp
@@ -6,10 +6,31 @@
 namespace EVT::core::DEV {
 class Encoder {
 public:
+    /**
+     * Constructs an Encoder instance. Call update() within the main loop to update encoder values.
+     * @param a a pin of the encoder
+     * @param b b pin of the encoder
+     * @param range range of the encoder positions (should be positive)
+     * @param initialPosition initial position the encoder is in
+     */
     Encoder(IO::GPIO& a, IO::GPIO& b, int64_t range, int64_t initialPosition);
 
-    int64_t update();
+    /**
+     * Reads and updates the encoder rotation and the absolute position.
+     * @return what position was changed by, should be in this range: [-3,3]
+     */
+    int8_t update();
 
+    /**
+     * returns the current absolute position
+     * @return the current position of the encoder, between -range and range, inclusive
+     */
+    int64_t getPosition();
+
+    /**
+     * An Enum that represents the direction the encoder is being spun in.
+     * The direction should reset to Static after a couple of reads with no changes.
+     */
     enum Direction {
         Left = -1,
         Static = 0,
@@ -24,9 +45,51 @@ private:
     int64_t range;
     int64_t position;
     Direction currentDirection;
-    int noChangeCounter;
+    uint8_t noChangeCounter;
+    int8_t currentRelPos;
+    const static int8_t noChangeCap = 2;
 
+    /**
+     * helper method to convert binary pin values to the relative rotation of the encoder, in the range [0,3]
+     * @param a value of the a pin
+     * @param b value of the b pin
+     * @return relative position of the encoder
+     */
     static int8_t convertPinValuesToPosition(bool a, bool b);
+
+    /**
+     * reads pin values and converts them to the relative position
+     * @return relative position of the encoder
+     */
+    int8_t readPinValues();
+
+    /**
+     * sets direction, setting noChangeCounter to 0 if the direction is set to Static
+     * @param newDirection value direction will be set to
+     */
+    void setDirection(Direction newDirection);
+
+    /**
+     * increments the noChangeCounter, setting the direction to Static if noChangeCounter is > noChangeCap
+     * @return whether or not the direction was set to Static
+     */
+    bool incrementNoChangeCounter();
+
+    /**
+     * parses the raw change value and determines what the change should actually be based on the direction
+     * the encoder was already traveling in
+     * @param change the raw change value, should be newPos - currentRelPos
+     * @return the actual change value
+     */
+    int8_t parseChange(int8_t change);
+
+    /**
+     * sets position to newPosition, capping it if the new value exceeds the range.
+     * @param newPosition the new value for position
+     * @return if position was capped
+     */
+    bool setPosition(int64_t newPosition);
+
 }; // namespace EVT::core::DEV
 } // namespace EVT::core::DEV
 
--- src/dev/encoder.cpp
+++ src/dev/encoder.cpp
@@ -3,21 +3,105 @@
 namespace EVT::core::DEV {
 
 Encoder::Encoder(IO::GPIO& a, IO::GPIO& b, int64_t range, int64_t initialPosition): a(a), b(b), range(range), position(initialPosition){
+    //making sure range is positive
     if (range < 0) {
         range *= -1;
     }
+    //making sure position is in the range
     if (position > range) {
         position = range;
     } else if (position < -1*range) {
         position = -1*range;
     }
+    //setting other instance variables
+    currentDirection = Direction::Static;
+    noChangeCounter = 0;
+    currentRelPos = readPinValues();
 }
 
-int64_t Encoder::update() {
+int64_t Encoder::getPosition() {
+    return position;
+}
+
+
+
+int8_t Encoder::update() {
+    //Reads current pin values
+    int8_t newPos = readPinValues();
+    //find the change in values;
+    int8_t change = newPos - currentRelPos;
+    change = parseChange(change);
+    setPosition(position + change);
+    return change;
+}
+
+int8_t Encoder::parseChange(int8_t change) {
+    if (currentDirection == Static) {
+        switch(change) {
+        case -1:
+            setDirection(Left);
+            break;
+        case 1:
+            setDirection(Right);
+            break;
+        case -3:
+            setDirection(Right);
+            change = 1;
+            break;
+        case 3:
+            setDirection(Left);
+            change = -1;
+            break;
+        default:
+            change = 0;
+        }
+        return change;
+    }
+    if (change == 0) {
+        incrementNoChangeCounter();
+        return change;
+    } else {
+        noChangeCounter = 0;
+    }
+    if (currentDirection == Left) {
+        if (change < 0) {
+            return change;
+        } else {
+            change *= -1;
+            return change;
+        }
+    }
+    if (currentDirection == Right) {
+        if (change < 0) {
+            change *= -1;
+            return change;
+        } else {
+            return change;
+        }
+    }
+}
+
+void Encoder::setDirection(Direction newDirection) {
+        if (newDirection == Static) {
+            noChangeCounter = 0;
+        }
+        currentDirection = newDirection;
+}
+
+bool Encoder::incrementNoChangeCounter() {
+        noChangeCounter++;
+        if (noChangeCounter >= noChangeCap) {
+            setDirection(Static);
+            return true;
+        }
+        return false;
+}
+
+int8_t Encoder::readPinValues() {
     bool aPos = (bool)a.readPin();
     bool bPos = (bool)b.readPin();
     int8_t newPos = convertPinValuesToPosition(aPos,bPos);
-
+    return newPos;
 }
 
 int8_t Encoder::convertPinValuesToPosition(bool a, bool b) {
@@ -25,6 +109,17 @@ int8_t Encoder::convertPinValuesToPosition(bool a, bool b) {
     if (a == 1 && b == 0) {return 1;}
     if (a == 1 && b == 1) {return 2;}
     if (a == 0 && b == 1) {return 3;}
+}
+
+bool Encoder::setPosition(int64_t newPosition) {
+    if (newPosition > range) {
+            newPosition = range;
+            return true;
+    } else if (newPosition < range) {
+            newPosition = -1 * range;
+            return true;
+    }
+    return false;
 }
 
 } // namespace EVT::core::DEV
