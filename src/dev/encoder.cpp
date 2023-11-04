5b54850f554241f40ca162400373b9b1f8ca7fd2
GitHub Build <N/A>
2023-11-04 16:39:04 +0000

    Applied Formatting Changes During GitHub Build

--- src/dev/encoder.cpp
+++ src/dev/encoder.cpp
@@ -2,10 +2,8 @@
 
 namespace EVT::core::DEV {
 
-
-
 Encoder::Encoder(IO::GPIO& a, IO::GPIO& b, uint64_t range, uint64_t initialPosition, bool rollOver)
-    : a(a), b(b), range(range), position(initialPosition), rollOver(rollOver){
+    : a(a), b(b), range(range), position(initialPosition), rollOver(rollOver) {
     if (position > range) {
         position = range;
     }
@@ -16,30 +14,29 @@ Encoder::Encoder(IO::GPIO& a, IO::GPIO& b, uint64_t range, uint64_t initialPosit
     recentBInterruptTime = 0;
 }
 
-
 void Encoder::aInterruptHandler(IO::GPIO* pin) {
     if ((time::millis() - recentAInterruptTime) < INTERRUPTCOOLDOWN) {
         log::LOGGER.log(log::Logger::LogLevel::DEBUG, "aInterrupt Skipped, reason: on cooldown");
         return;
     }
     int8_t change;
-    switch(currentRelPos) {
-        case 0:
-            change = 1;
-            currentRelPos = 1;
-            break;
-        case 1:
-            change = -1;
-            currentRelPos = 0;
-            break;
-        case 2:
-            change = 1;
-            currentRelPos = 3;
-            break;
-        case 3:
-            change = -1;
-            currentRelPos = 2;
-            break;
+    switch (currentRelPos) {
+    case 0:
+        change = 1;
+        currentRelPos = 1;
+        break;
+    case 1:
+        change = -1;
+        currentRelPos = 0;
+        break;
+    case 2:
+        change = 1;
+        currentRelPos = 3;
+        break;
+    case 3:
+        change = -1;
+        currentRelPos = 2;
+        break;
     }
     interruptChange += change;
     recentAInterruptTime = time::millis();
@@ -47,39 +44,41 @@ void Encoder::aInterruptHandler(IO::GPIO* pin) {
     log::LOGGER.log(log::Logger::LogLevel::DEBUG, "aInterrupt Called, "
                                                   "\n\r\tCalculated Position: %d"
                                                   "\n\r\tActual Position: %d"
-                                                  "\n\r\tChange: %d",currentRelPos, readPinValues(), change);
+                                                  "\n\r\tChange: %d",
+                    currentRelPos, readPinValues(), change);
 }
 
 void Encoder::bInterruptHandler(IO::GPIO* pin) {
     if ((time::millis() - recentBInterruptTime) < INTERRUPTCOOLDOWN) {
-            log::LOGGER.log(log::Logger::LogLevel::DEBUG, "bInterrupt Skipped, reason: on cooldown");
-            return;
+        log::LOGGER.log(log::Logger::LogLevel::DEBUG, "bInterrupt Skipped, reason: on cooldown");
+        return;
     }
     int8_t change;
-    switch(currentRelPos) {
+    switch (currentRelPos) {
     case 0:
-            change = -1;
-            currentRelPos = 3;
-            break;
+        change = -1;
+        currentRelPos = 3;
+        break;
     case 1:
-            change = 1;
-            currentRelPos = 2;
-            break;
+        change = 1;
+        currentRelPos = 2;
+        break;
     case 2:
-            change = -1;
-            currentRelPos = 1;
-            break;
+        change = -1;
+        currentRelPos = 1;
+        break;
     case 3:
-            change = 1;
-            currentRelPos = 0;
-            break;
+        change = 1;
+        currentRelPos = 0;
+        break;
     }
     interruptChange += change;
     recentBInterruptTime = time::millis();
     log::LOGGER.log(log::Logger::LogLevel::DEBUG, "bInterrupt Called, "
                                                   "\n\r\tCalculated Position: %d"
                                                   "\n\r\tActual Position: %d"
-                                                  "\n\r\tChange: %d",currentRelPos, readPinValues(), change);
+                                                  "\n\r\tChange: %d",
+                    currentRelPos, readPinValues(), change);
 }
 
 uint64_t Encoder::getPosition() const {
@@ -95,43 +94,48 @@ void Encoder::update() {
 }
 
 int8_t Encoder::readPinValues() {
-    bool aPos = (bool)a.readPin();
-    bool bPos = (bool)b.readPin();
-    int8_t newPos = convertPinValuesToPosition(aPos,bPos);
+    bool aPos = (bool) a.readPin();
+    bool bPos = (bool) b.readPin();
+    int8_t newPos = convertPinValuesToPosition(aPos, bPos);
     return newPos;
 }
 
 int8_t Encoder::convertPinValuesToPosition(bool a, bool b) {
-    if (a == 0 && b == 0) {return 0;}
-    else if (a == 1 && b == 0) {return 1;}
-    else if (a == 1 && b == 1) {return 2;}
-    else if (a == 0 && b == 1) {return 3;}
+    if (a == 0 && b == 0) {
+        return 0;
+    } else if (a == 1 && b == 0) {
+        return 1;
+    } else if (a == 1 && b == 1) {
+        return 2;
+    } else if (a == 0 && b == 1) {
+        return 3;
+    }
 }
 
 bool Encoder::changePosition(int64_t change) {
-    change %= (int64_t)range;
+    change %= (int64_t) range;
     bool hitCap = false;
-    int64_t newPosition = (int64_t)position;
+    int64_t newPosition = (int64_t) position;
     newPosition += change;
     if (newPosition < 0) {
-            if (rollOver) {
-                position = (int64_t)(range)+newPosition+1;
-            } else {
-                position = 0;
-            }
-            hitCap = true;
-    } else if (newPosition > (int64_t)range) {
-            //Position reached cap
-            if (rollOver) {
-                position = (uint64_t)newPosition -1 - range;
-            } else {
-                position = range;
-            }
-            hitCap = true;
+        if (rollOver) {
+            position = (int64_t) (range) + newPosition + 1;
+        } else {
+            position = 0;
+        }
+        hitCap = true;
+    } else if (newPosition > (int64_t) range) {
+        //Position reached cap
+        if (rollOver) {
+            position = (uint64_t) newPosition - 1 - range;
+        } else {
+            position = range;
+        }
+        hitCap = true;
     } else {
-            position = (uint64_t)newPosition;
+        position = (uint64_t) newPosition;
     }
     return hitCap;
 }
 
-} // namespace EVT::core::DEV
+}// namespace EVT::core::DEV
