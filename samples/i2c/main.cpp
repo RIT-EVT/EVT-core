4cca0890a82eed9803018293a322c6c6effdaadb
Ishan Renjen <66697119+ishan-renjen@users.noreply.github.com>
2023-12-09 12:44:41 -0500

    fixed changed to i2c and IWDG samples

--- samples/i2c/main.cpp
+++ samples/i2c/main.cpp
@@ -18,6 +18,7 @@ namespace time = EVT::core::time;
 
 /** The address of the arduino listening for I2C requests */
 constexpr uint8_t I2C_SLAVE_ADDR = 0x48;
+//Address is 0x48 for temp sensors
 /** The "register" location of the "o" byte */
 constexpr uint8_t O_REGISTER = 0x00;
 /** The "register" location of the "k" byte */
@@ -27,36 +28,37 @@ int main() {
     // Initialize system
     EVT::core::platform::init();
 
+    IO::I2C& i2c = IO::getI2C<IO::Pin::PB_8, IO::Pin::PB_9>();
     IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
-
-    uart.printf("no I2C test\n\r");
-
-//    HAL_Init();
-
-    IO::I2C& i2c = IO::getI2C<IO::Pin::I2C_SCL, IO::Pin::I2C_SDA>();
-
 
     uart.printf("Starting I2C test\n\r");
 
-
-//    while (1) {
+    while (1) {
         uart.printf("Requesting first byte\n\r");
 
         // Read the value of 'o'
-        uint8_t temp[2];
-        uint8_t reg = 0x00;
-        IO::I2C::I2CStatus status = i2c.readReg(I2C_SLAVE_ADDR, &reg, 1, temp, 2);
+        uint8_t oValue;
+        IO::I2C::I2CStatus status = i2c.readReg(I2C_SLAVE_ADDR, O_REGISTER, &oValue);
         if (status != IO::I2C::I2CStatus::OK) {
             uart.printf("Failed read 'o' register with I2C::I2CStatus: %d\n\r",
                         status);
-//            break;
+            break;
         }
 
-        uart.printf("O value: %#x, %#x\n\r", temp[0], temp[1]);
+        uart.printf("Reading second bytes\n\r");
+
+        // Read the value of 'k'
+        uint8_t kValue;
+        status = i2c.readReg(I2C_SLAVE_ADDR, K_REGISTER, &kValue);
+        if (status != IO::I2C::I2CStatus::OK) {
+            uart.printf("Failed read 'k' register with I2C::I2CStatus: %d\n\r",
+                        status);
+            break;
+        }
+
+        uart.printf("Bytes Read: %c %c\n\r", oValue, kValue);
 
         // Wait half a second before repeating the test
         time::wait(500);
-//        break;
-//    }
-    return 0;
+    }
 }
--- samples/iwdg/main.cpp
+++ samples/iwdg/main.cpp
@@ -1,7 +1,7 @@
 /**
- * Example of IWDG functionality. The timer is refreshed 3 times in a loop,
- * then hits an infinite loop in the code which triggers a system reset.
- */
+* Example of IWDG functionality. The timer is refreshed 3 times in a loop,
+* then hits an infinite loop in the code which triggers a system reset.
+*/
 #include <EVT/manager.hpp>
 #include <EVT/utils/time.hpp>
 
@@ -10,24 +10,23 @@ namespace DEV = EVT::core::DEV;
 namespace time = EVT::core::time;
 
 int main() {
-    // Initialize system
-    EVT::core::platform::init();
-    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+   // Initialize system
+   EVT::core::platform::init();
+   IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
-    // Start watchdog
-    DEV::IWDG& iwdg = DEV::getIWDG(5000);
-    iwdg.init();
+   // Start watchdog
+   DEV::IWDG& iwdg = DEV::getIWDG(5000);
+   iwdg.init();
 
-    uart.printf("Starting IWDG test...\n\r\n\r");
+   uart.printf("Starting IWDG test...\n\r\n\r");
 
-    // Refresh watchdog at regular intervals
-    for (int i = 0; i < 3; i++) {
-        time::wait(500);
-        iwdg.refresh();
-        uart.printf("IWDG refreshed\n\r");
-    }
+   // Refresh watchdog at regular intervals
+   for (int i = 0; i < 3; i++) {
+       time::wait(500);
+       iwdg.refresh();
+       uart.printf("IWDG refreshed\n\r");
+   }
 
-    // Infinite loop represents an error, board should reset after the watchdog times out
-    while (1)
-        ;
+   // Infinite loop represents an error, board should reset after the watchdog times out
+   while (1);
 }
