fd768e0f62f86d5d9e48f38f8cf186aab1709dc2
tmb5932 <tmb5932@g.rit.edu>
2024-03-02 15:10:21 -0500

    DMA and ADC works, but only reads once.

--- samples/adc/multi_adc.cpp
+++ samples/adc/multi_adc.cpp
@@ -22,18 +22,12 @@ int main() {
     time::wait(500);
 
     IO::ADC& adc0 = IO::getADC<IO::Pin::PA_0>();
-    IO::ADC& adc1 = IO::getADC<IO::Pin::PA_1>();
 
     while (1) {
         uart.printf("--------------------\r\n");
         uart.printf("ADC0 : %d mV\r\n", static_cast<uint32_t>(adc0.read() * 1000));
         uart.printf("ADC0: %d%%\r\n", static_cast<uint32_t>(adc0.readPercentage() * 100));
         uart.printf("ADC0 raw: %d\r\n\r\n", adc0.readRaw());
-
-        uart.printf("ADC1 : %d mV\r\n", static_cast<uint32_t>(adc1.read() * 1000));
-        uart.printf("ADC1: %d%%\r\n", static_cast<uint32_t>(adc1.readPercentage() * 100));
-        uart.printf("ADC1 raw: %d\r\n", adc1.readRaw());
-
         uart.printf("--------------------\r\n\r\n");
         time::wait(500);
     }
