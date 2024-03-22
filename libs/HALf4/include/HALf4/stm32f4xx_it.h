9243ef4aeba78802ae739d670cce5898309ea498
tmb5932 <tmb5932@g.rit.edu>
2024-03-21 23:12:20 -0400

    Reverted file

--- libs/HALf4/include/HALf4/stm32f4xx_it.h
+++ libs/HALf4/include/HALf4/stm32f4xx_it.h
@@ -39,7 +39,6 @@ void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void SysTick_Handler(void);
-void DMA2_Stream0_IRQHandler(void);
 
 #ifdef __cplusplus
 }
