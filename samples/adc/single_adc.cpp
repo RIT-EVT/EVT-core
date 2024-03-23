2430772c4702f2ad0aca73ac49a5e5d8e9604df0
tmb5932 <tmb5932@g.rit.edu>
2024-03-23 10:46:47 -0400

    Fixed weird improper spacing when copying the samples back into their files from github

--- samples/adc/single_adc.cpp
+++ samples/adc/single_adc.cpp
@@ -1,8 +1,8 @@
 /**
-* This example shows off the use of the ADC. Two pins are setup for ADC
-* functionality and the values are continuously read in and printed over
-* UART.
-*/
+ * This example shows off the use of the ADC. Two pins are setup for ADC
+ * functionality and the values are continuously read in and printed over
+ * UART.
+ */
 #include <EVT/io/ADC.hpp>
 #include <EVT/io/UART.hpp>
 #include <EVT/manager.hpp>
@@ -12,23 +12,23 @@ namespace IO = EVT::core::IO;
 namespace time = EVT::core::time;
 
 int main() {
-   // Initialize system
-   EVT::core::platform::init();
+    // Initialize system
+    EVT::core::platform::init();
 
-   IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
-   uart.printf("Starting ADC test\r\n");
+    uart.printf("Starting ADC test\r\n");
 
-   time::wait(500);
+    time::wait(500);
 
-   IO::ADC& adc0 = IO::getADC<IO::Pin::PA_0>();
+    IO::ADC& adc0 = IO::getADC<IO::Pin::PA_0>();
 
-   while (1) {
-       uart.printf("--------------------\r\n");
-       uart.printf("ADC0 : %d mV\r\n", static_cast<uint32_t>(adc0.read() * 1000));
-       uart.printf("ADC0: %d%%\r\n", static_cast<uint32_t>(adc0.readPercentage() * 100));
-       uart.printf("ADC0 raw: %d\r\n\r\n", adc0.readRaw());
-       uart.printf("--------------------\r\n\r\n");
-       time::wait(500);
-   }
+    while (1) {
+        uart.printf("--------------------\r\n");
+        uart.printf("ADC0 : %d mV\r\n", static_cast<uint32_t>(adc0.read() * 1000));
+        uart.printf("ADC0: %d%%\r\n", static_cast<uint32_t>(adc0.readPercentage() * 100));
+        uart.printf("ADC0 raw: %d\r\n\r\n", adc0.readRaw());
+        uart.printf("--------------------\r\n\r\n");
+        time::wait(500);
+    }
 }
