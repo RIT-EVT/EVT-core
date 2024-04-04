c2b1349a2c15f051c9dde6fe06687eada83767d2
tmb5932 <tmb5932@g.rit.edu>
2024-04-04 19:39:47 -0400

    WORKS F4 and F3 :). Tested with SPI1, and had no issues that I can find.

--- include/EVT/io/pin.hpp
+++ include/EVT/io/pin.hpp
@@ -130,7 +130,7 @@ enum class Pin {
 #ifdef STM32F4xx
     SPI_SCK = PA_5, // D13
     SPI_MISO = PA_6,// D12
-    SPI_MOSI = PA_7,// D11 // todo test with these pins :)
+    SPI_MOSI = PA_7,// D11
     SPI_CS = PB_6,  // D10
     I2C_SDA = PB_9,
     I2C_SCL = PB_8,
