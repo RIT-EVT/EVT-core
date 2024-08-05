15d4a091fd6e03dc5382e3daab378368bda3348a
Rue <xavierfinch05@gmail.com>
2024-08-05 11:56:04 -0400

    added bytepool header.

---
+++ include/EVT/rtos/BytePool.hpp
@@ -0,0 +1,15 @@
+
+#ifndef _EVT_RTOS_BYTEPOOL_
+#define _EVT_RTOS_BYTEPOOL_
+
+namespace core::rtos {
+
+class BytePool {
+public:
+
+};
+
+} //namespace core::rtos
+
+
+#endif //_EVT_RTOS_BYTEPOOL_
