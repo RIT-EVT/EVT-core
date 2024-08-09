7c0e8c78b8e9eac4122ba1d6aaae52f8a0dc8ae8
Rue <xavierfinch05@gmail.com>
2024-08-09 13:29:40 -0400

    implemented initial queue functions.

--- include/EVT/rtos/Queue.hpp
+++ include/EVT/rtos/Queue.hpp
@@ -9,6 +9,12 @@ class Queue : Initializable {
 public:
 
     /**
+     * A type that represents the function that can be registered to a queue
+     * to notify when it is sent a message.
+     */
+    typedef void(*notifyFunction_t)(TX_QUEUE*);
+
+    /**
      * Constructs a Queue object, but does not initiaize it (must call init before using).
      *
      * @param name The name of the queue.
@@ -17,7 +23,25 @@ public:
      */
     Queue(const char* name, UINT messageSize,  ULONG queueSize);
 
+    UINT destroy();
 
+    UINT flush();
+
+    UINT prioritize();
+
+    UINT recieve(void* destination, ULONG waitOption);
+
+    /**
+     * Register a function to be called whenever a message is successfully sent to the queue.
+     *
+     * @param notifyFunction The function to be called when a message is sent to the queue.
+     * @return A Threadx status code representing the success of the method.
+     */
+    UINT registerSendNotifyFunction(notifyFunction_t notifyFunction);
+
+    UINT send(void* source, ULONG waitOption);
+
+    UINT frontSend(void* source, ULONG waitOption);
 
     bool init(BytePool &pool) override;
 private:
--- src/rtos/Queue.cpp
+++ src/rtos/Queue.cpp
@@ -6,6 +6,34 @@ namespace core::rtos {
     Queue::Queue(const char* name, UINT messageSize, ULONG queueSize)
         : name(name), messageSize(messageSize), queueSize(queueSize) {}
 
+    UINT Queue::destroy() {
+        tx_queue_delete(&txQueue);
+    }
+
+    UINT Queue::flush() {
+        tx_queue_flush(&txQueue);
+    }
+
+    UINT Queue::prioritize() {
+        tx_queue_prioritize(&txQueue);
+    }
+
+    UINT Queue::recieve(void* destination, ULONG waitOption) {
+        tx_queue_receive(&txQueue, destination, waitOption);
+    }
+
+    UINT Queue::registerSendNotifyFunction(core::rtos::Queue::notifyFunction_t notifyFunction) {
+        tx_queue_send_notify(&txQueue, notifyFunction);
+    }
+
+    UINT Queue::send(void* source, ULONG waitOption) {
+        tx_queue_send(&txQueue, source, waitOption);
+    }
+
+    UINT Queue::frontSend(void* source, ULONG waitOption) {
+        tx_queue_front_send(&txQueue, source, waitOption);
+    }
+
     bool Queue::init(core::rtos::BytePool& pool) {
         void* poolPointer = pool.AllocateMemory(queueSize, TX_NO_WAIT);
         tx_queue_create(&txQueue, name, messageSize, poolPointer, queueSize);
