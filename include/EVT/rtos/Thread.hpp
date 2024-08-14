0594ca7cbaa9c82feda6143bdd17b15ceb3582d3
Rue <xavierfinch05@gmail.com>
2024-08-14 16:54:17 -0400

    Stubbed out all necessary methods for Thread, implemented the constructor and deconstructor.

--- include/EVT/rtos/Thread.hpp
+++ include/EVT/rtos/Thread.hpp
@@ -10,35 +10,150 @@ namespace core::rtos {
 template <typename T>
 class Thread : Initializable {
 public:
+
+    /**
+     * A type that represents the function that can be registered to a thread
+     * to notify when it starts or ends.
+     */
+    typedef void(*threadNotifyFunction_t)(TX_THREAD*, uint32_t);
+
     /**
      * Constructor for a Thread object. Thread will not start until init() method is called.
      *
-     * @param entryFunction Function the thread will be running.
-     * @param data Data the thread's function requires.
-     * @param stackSize How much stack space this thread is allocated.
+     * @param[in] name pointer to a null-terminated character string representing the name of the thread
+     * @param[in] entryFunction Function the thread will be running.
+     * @param[in] data Data the thread's function requires.
+     * @param[in] stackSize How much stack space this thread is allocated.
+     * @param[in] priority The initial priority of the thread (lower value = higher priority).
+     * @param[in] preemptThreshold The value of the preemption threshold of the thread.
+     * @param[in] timeSlice How much time the thread will run before the scheduler may switch to another thread.
+     * @param[in] autoStart Whether the thread starts as soon as it is initialized or is created suspended.
      */
-    Thread(void (*entryFunction)(T*), T &data, std::size_t stackSize);
+    Thread(const char* name, void (*entryFunction)(T*), T* data, std::size_t stackSize, uint32_t priority,
+           uint32_t preemptThreshold, uint32_t timeSlice, bool autoStart);
 
+    /**
+     * Thread Deconstructor
+     */
+    ~Thread();
 
+    /**
+     * Registers a function to be called when the thread initially is entered
+     * and when the thread completes or is terminated.
+     *
+     * @param[in] notifyFunction The function that will be called.
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError registerEntryExitNotification(threadNotifyFunction_t notifyFunction);
+
+    //TODO: do we really need a thread info get function?
+
+    /**
+     * Sets the preemptThreshold of the thread to the new values specified.
+     *
+     * @param[in] newThreshold The new value that the thread's preemptThreshold will be set to.
+     * @param[out] oldThresholdOut A pointer to a space to store the previous value of the preemptThreshold
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError setPreemptThreshold(uint32_t newThreshold, uint32_t* oldThresholdOut);
+
+    /**
+     * Sets the priority of the thread to the new value specified.
+     *
+     * @param[in] newPriority The new value that the thread's priority will be set to.
+     * @param[out] oldPriorityOut A pointer to a space to store the previous value of the priority.
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError setPriority(uint32_t newPriority, uint32_t* oldPriorityOut);
+
+    //TODO: tx_thread_relinquish exists in tx_api.h, but doesn't seem to have an implementation... Figure out if it is needed/
+
+    /**
+     * Sets the timeSlice of the thread to the new value specified.
+     *
+     * @param[in] newTimeSlice The new value that the thread's timeSlice will be set to.
+     * @param[out] oldTimeSliceOutput A pointer to a space to store the previous value of the timeSlice.
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError setTimeSlice(uint32_t newTimeSlice, uint32_t* oldTimeSliceOut);
+
+    /**
+     * Resets the thread so it is prepared to run the entry function again.
+     *
+     * @return Enum representing the first error found by the function (or Success if there was no error).
+     */
+    TXError reset();
+
+    /**
+     * Resumes the thread if it is suspended.
+     *
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError resume();
+
+    /**
+     * Suspends the thread.
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError suspend();
+
+    /**
+     * Terminates the thread. Once this thread is terminated, it cannot be restarted.
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError terminate();
+
+    /**
+     * Aborts whatever wait this thread is in, returning WaitAborted to the method that this thread was waiting in.
+     *
+     * @return The first error found by the function (or Success if there was no error).
+     */
+    TXError abortWait();
 
     /**
      * Creates the threadx thread and starts it.
      * @return whether or not the thread was started successfully.
      */
     TXError init(BytePoolBase &pool) override;
+
+
 private:
+    /**
+     * Threadx struct that holds the information for the thread.
+     */
+    TX_THREAD txThread;
+    /**
+     * Pointer to the name of this thread.
+     */
+    const char* name;
     /**
      * The function this thread will be running.
      */
-    void (*entryFunction)(T);
+    void (*entryFunction)(T*);
     /**
      * The data the thread function requires.
      */
-    T data;
+    T* data;
     /**
      * How much stack space the thread requires.
      */
     std::size_t stackSize;
+    /**
+     * The priority rating of this thread.
+     */
+    uint32_t priority;
+    /**
+     * The preemption rating of this thread.
+     */
+    uint32_t preemptThreshold;
+    /**
+     * The time slice of this thread.
+     */
+    uint32_t timeSlice;
+    /**
+     * Whether this thread will start when initialized or not.
+     */
+    bool autoStart;
 };
 
 } // namespace core::rtos
--- src/rtos/Thread.cpp
+++ src/rtos/Thread.cpp
@@ -3,8 +3,30 @@
 
 namespace core::rtos {
 
-    Thread::init(BytePool &pool) {
+template<typename T>
+Thread<T>::Thread(const char* name, void (*entryFunction)(T*), T* data, std::size_t stackSize,
+                  uint32_t priority, uint32_t preemptThreshold, uint32_t timeSlice, bool autoStart)
+    : name(name), entryFunction(entryFunction), data(data), stackSize(stackSize), priority(priority),
+        preemptThreshold(preemptThreshold), timeSlice(timeSlice), autoStart(autoStart) {}
 
-    }
+
+/**
+ * TODO: should the deconstructor call terminate() before calling delete?
+ * Otherwise if the thread is not finished, the delete function will not actually work
+ */
+template<typename T>
+Thread<T>::~Thread() {
+    tx_thread_delete(&txThread);
+}
+
+template<typename T>
+TXError Thread<T>::init(core::rtos::BytePoolBase& pool) {
+    void *stackStart;
+    stackStart = pool.AllocateMemory(stackSize, 0);
+    tx_thread_create(&txThread, name, entryFunction, data, stackStart,
+                     stackSize, priority, preemptThreshold, timeSlice, autoStart ? TX_AUTO_START : TX_DONT_START);
+}
+
+
 
 } //namespace core::rtos
