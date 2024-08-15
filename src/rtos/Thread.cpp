ed5540c0d82ac430eef972f3bc59a55a7f38de34
Rue <xavierfinch05@gmail.com>
2024-08-15 18:36:30 -0400

    Implemented all Thread methods in Thread.cpp.

--- src/rtos/Thread.cpp
+++ src/rtos/Thread.cpp
@@ -27,6 +27,52 @@ TXError Thread<T>::init(core::rtos::BytePoolBase& pool) {
                      stackSize, priority, preemptThreshold, timeSlice, autoStart ? TX_AUTO_START : TX_DONT_START);
 }
 
+template<typename T>
+TXError Thread<T>::registerEntryExitNotification(threadNotifyFunction_t notifyFunction) {
+    tx_thread_entry_exit_notify(&txThread, notifyFunction);
+}
 
+template<typename T>
+TXError Thread<T>::setPreemptThreshold(uint32_t newThreshold, uint32_t* oldThresholdOut) {
+    preemptThreshold = newThreshold;
+    tx_thread_preemption_change(&txThread, newThreshold, oldThresholdOut);
+}
+
+template<typename T>
+TXError Thread<T>::setPriority(uint32_t newPriority, uint32_t* oldPriorityOut) {
+    priority = newPriority;
+    tx_thread_priority_change(&txThread, newPriority, oldPriorityOut);
+}
+
+template<typename T>
+TXError Thread<T>::setTimeSlice(uint32_t newTimeSlice, uint32_t* oldTimeSliceOut) {
+    timeSlice = newTimeSlice;
+    tx_thread_time_slice_change(&txThread, newTimeSlice, oldTimeSliceOut);
+}
+
+template<typename T>
+TXError Thread<T>::reset() {
+    tx_thread_reset(&txThread);
+}
+
+template<typename T>
+TXError Thread<T>::resume() {
+    tx_thread_resume(&txThread);
+}
+
+template<typename T>
+TXError Thread<T>::suspend() {
+    tx_thread_suspend(&txThread);
+}
+
+template<typename T>
+TXError Thread<T>::terminate() {
+    tx_thread_terminate(&txThread);
+}
+
+template<typename T>
+TXError Thread<T>::abortWait() {
+    tx_thread_wait_abort(&txThread)
+}
 
 } //namespace core::rtos
