3b2264094bb354a4270184828f8363275db38a83
mjh9585 <69865851+mjh9585@users.noreply.github.com>
2024-09-14 12:22:12 -0400

    F3xx 32MHz clock and ADC fix

--- src/core/platform/f3xx/stm32f302x8.cpp
+++ src/core/platform/f3xx/stm32f302x8.cpp
@@ -20,13 +20,13 @@ void stm32f3xx_init() {
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLL_MUL4; // PLL CLK at 8 * 4 = 32 MHz
+    RCC_OscInitStruct.PLL.PLLMUL          = RCC_PLL_MUL8; // PLL CLK at 8 / 2 * 8 = 32 MHz
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
     /** Initializes the CPU, AHB and APB buses clocks
      */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI; // SYSCLK at 8 MHz (HSI CLK)
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK; // SYSCLK at 32 MHz (PLL CLK)
     RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
