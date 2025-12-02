409b85f40ea9d123f6e3943467ea38ef22db9a5f
tmb5932 <tmb5932@g.rit.edu>
2025-12-01 21:19:25 -0500

    updated clock to run faster, now achieving the 3ms required model tick time.

--- src/core/platform/f4xx/stm32f446xx.cpp
+++ src/core/platform/f4xx/stm32f446xx.cpp
@@ -3,7 +3,6 @@
 #include <HALf4/stm32f4xx_it.h>
 
 #include <core/platform/f4xx/stm32f4xx.hpp>
-#include <iostream>
 
 using namespace std;
 namespace core::platform {
@@ -28,24 +27,23 @@ void stm32f4xx_init() {
     RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
     RCC_OscInitStruct.PLL.PLLM            = 8;
-    RCC_OscInitStruct.PLL.PLLN            = 50;
+    RCC_OscInitStruct.PLL.PLLN            = 100;
     RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV2;
     RCC_OscInitStruct.PLL.PLLQ            = 2;
     RCC_OscInitStruct.PLL.PLLR            = 2;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
-    //    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    //    {
-    //        Error_Handler();
-    //    }
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
     /** Initializes the CPU, AHB and APB buses clocks
      */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK; // SYSCLK at 50 MHz (HSI CLK)
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK; // SYSCLK at 100 MHz (HSI CLK)
     RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
         Error_Handler();
     }
 
